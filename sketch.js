07dcdcc42a0b48971f8a0d33640e4e701cf2bf1f
Johaan-Paul <johaanp2008@gmail.com>
2022-01-03 17:42:11 +1300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,137 @@
+var backgroundImg, runningManImg, collidedManImg, stoneImg, gameOverImg, restartImg;
+var collidedSound, jumpSound;
+var backGround, man, stonesGroup, trashsGroup, invisibleGround, gameOver, restart;
+var distance, speed, score, gameState, play, end;
+function preload() {
+  backgroundImg = loadImage("./assets/full_background.png");
+  gameOverImg = loadImage("./assets/gameOver.png");
+  restartImg = loadImage("./assets/restart.png");
+  stoneImg = loadImage("./assets/stone.png");
+  trash1Img = loadImage("./assets/Trash 1.jfif");
+  trash2Img = loadImage("./assets/Trash 2.png");
+  runningManImg = loadAnimation("./assets/Running Man Frame 1.png", "./assets/Running Man Frame 2.png", "./assets/Running Man Frame 3.png", "./assets/Running Man Frame 4.png");
+  collidedManImg = loadAnimation("./assets/Running Man Frame 2.png");
+  collidedSound = loadSound("./assets/collided.wav");
+  jumpSound = loadSound("./assets/jump.wav");
+}
+
+function setup() {
+  console.log("Game BY:");
+  console.log("Johaan Paul");
+  createCanvas(window.innerWidth,window.innerHeight);
+
+  backGround = createSprite(window.innerWidth /2, window.innerHeight /2, window.innerWidth, window.innerHeight);
+  backGround.scale = 0.55;
+  backGround.addImage(backgroundImg);
+
+  man = createSprite(window.innerWidth /4, window.innerHeight /1.3, window.innerWidth, window.innerHeight);
+  man.scale = 2.5;
+  man.addAnimation("running", runningManImg);
+  man.addAnimation("collided", collidedManImg);
+  man.setCollider("rectangle", 0, 0);
+  //man.debug = true;
+
+  stonesGroup = new Group();
+
+  trashsGroup = new Group();
+
+  gameOver = createSprite(window.innerWidth /2, window.innerHeight /2, window.innerWidth, window.innerHeight);
+  gameOver.scale = 2;
+  gameOver.addImage(gameOverImg);
+  gameOver.visible = false;
+
+  invisibleGround = createSprite(window.innerWidth /2, window.innerHeight /1.05, window.innerWidth *3, window.innerHeight /5);
+  invisibleGround.visible = false;
+  invisibleGround.setCollider("rectangle", 0, 0);
+  invisibleGround.debug = true;
+
+  restart = createSprite(window.innerWidth /2, window.innerHeight /1.5, window.innerWidth, window.innerHeight)
+  restart.scale = 0.15;
+  restart.addImage(restartImg);
+  restart.visible = false;
+
+  speed = -5;
+  distance = 0;
+  score = 0;
+  play = 0;
+  end = 1;
+  gameState = play;
+}
+
+function draw() {
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  background(0, 0, 0);
+  backGround.velocityX = speed;
+  if (gameState === play) {
+    if (backGround.x < window.innerWidth/3.2) {
+      backGround.x = window.innerWidth/2;
+    }
+    speed = speed -0.01;
+    distance = distance + 0.5
+
+    if(keyDown("space") && man.y >= window.innerHeight /1.35) {
+      jumpSound.play();
+      man.velocityY = -16;
+    }
+    
+    man.velocityY = man.velocityY + 0.8
+    man.collide(invisibleGround);
+    
+    spawnStones();
+    spawnTrash();
+    
+    if (stonesGroup.isTouching(man)) {
+      collidedSound.play();
+      gameState = end;
+    }
+    
+    if (trashsGroup.isTouching(man)) {
+      score = score +1;
+      trashsGroup.destroyEach();
+    }
+
+
+  }
+
+  else if (gameState === end) {
+    speed = 0;
+    
+    man.velocityY = 0;
+    stonesGroup.setVelocityXEach(0);
+    trashsGroup.setVelocityXEach(0);
+    man.changeAnimation("collided", collidedManImg);
+    gameOver.visible = true;
+
+  }
+  drawSprites();
+    textSize(20);
+    stroke(3);
+    fill("black");
+    text("score: " + score, window.innerWidth /2, 50);
+}
+
+function spawnStones() {
+  if (frameCount % 120 === 0) {
+    var stone = createSprite(window.innerWidth,window.innerHeight/1.2,40,40);
+    stone.setCollider("rectangle",0,0,200,200)
+    stone.addImage(stoneImg);
+    stone.velocityX = speed;
+    stone.scale = 0.15;
+    stone.lifetime = 400;
+    stonesGroup.add(stone);
+  }
+}
+
+function spawnTrash() {
+  if (frameCount % 150 === 0) {
+    var trash = createSprite(window.innerWidth,window.innerHeight/1.2,100,100);
+    trash.velocityX = speed
+    trash.scale = 2;
+    trash.addImage(trash2Img);
+    trash.scale = 0.05;
+    trash.lifetime = 400;
+    trash.setCollider("rectangle",0,0,trash.width/2,trash.height/2)
+    //trash.debug=true;
+    trashsGroup.add(trash);
+  }
+}
